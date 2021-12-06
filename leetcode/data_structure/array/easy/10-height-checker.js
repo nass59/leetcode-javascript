20710261d4d94d98f32f4ce06dc5c04feea4cbaf
Nassim Afrete <nassim.afrete@gmail.com>
2021-12-06 13:57:11 +0100

    [array][easy] - Height Checker

---
+++ leetcode/data_structure/array/easy/10-height-checker.js
@@ -0,0 +1,21 @@
+/**
+ * Time Complexity : O(n log n)
+ * Space Complexity : O(1)
+ *
+ * @param {number[]} heights
+ * @return {number}
+ */
+const heightChecker = heights => {
+  const sortedArray = [...heights].sort((a, b) => a - b)
+  let count = 0
+
+  for (let i = 0; i < heights.length; i++) {
+    if (heights[i] !== sortedArray[i]) count++
+  }
+
+  return count
+}
+
+console.log(heightChecker([1, 1, 4, 2, 1, 3]))
+console.log(heightChecker([5, 1, 2, 3, 4]))
+console.log(heightChecker([1, 2, 3, 4, 5]))
