e1e5c740ba10c7b1421b090bdd5b116db3a78f82
Nassim Afrete <nassim.afrete@gmail.com>
2021-12-06 14:04:35 +0100

    [array][easy] - Find Pivot Index

---
+++ leetcode/data_structure/array/easy/13-find-pivot-index.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const pivotIndex = nums => {
+  const total = nums.reduce((a, b) => a + b)
+  let sum = 0
+
+  for (let i = 0; i < nums.length; i++) {
+    if (sum * 2 === total - nums[i]) return i
+    sum += nums[i]
+  }
+
+  return -1
+}
+
+console.log(pivotIndex([-1, -1, -1, -1, 0, -1]))
+console.log(pivotIndex([-1, -1, -1, -1, -1, 0]))
+console.log(pivotIndex([1, 7, 3, 6, 5, 6]))
+console.log(pivotIndex([1, 2, 3]))
+console.log(pivotIndex([2, 1, -1]))
