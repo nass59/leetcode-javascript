cd750b45a52e4f85b76304b63009afc8b1c514f6
Nassim Afrete <nassim.afrete@gmail.com>
2021-12-07 15:12:09 +0100

    [array][medium] - Spiral Matrix

--- leetcode/data_structure/array/medium/03-spiral-matrix.js
+++ leetcode/data_structure/array/medium/03-spiral-matrix.js
@@ -15,14 +15,83 @@ const spiralOrder = matrix => {
       let val = m.pop()
 
       if (val) res.push(val)
-
       m.reverse()
     }
 
     matrix.reverse()
   }
-
   return res
+}
+
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+const spiralOrder = matrix => {
+  const result = []
+  const moves = new Map()
+  moves.set('up', 'right')
+  moves.set('right', 'down')
+  moves.set('down', 'left')
+  moves.set('left', 'up')
+
+  const m = matrix.length
+  const n = matrix[0].length
+
+  let row = 0
+  let column = 0
+
+  nextMove = moves.get('up')
+
+  const isNull = () => matrix[row][column] === null
+
+  while (nextMove !== null) {
+    if (nextMove === 'right') {
+      while (column < n && !isNull()) {
+        result.push(matrix[row][column])
+        matrix[row][column++] = null
+      }
+
+      row++
+      column--
+      nextMove = row < m ? moves.get('right') : null
+    }
+
+    if (nextMove === 'down') {
+      while (row < m && !isNull()) {
+        result.push(matrix[row][column])
+        matrix[row++][column] = null
+      }
+
+      row--
+      column--
+      nextMove = column >= 0 ? moves.get('down') : null
+    }
+
+    if (nextMove === 'left') {
+      while (column >= 0 && !isNull()) {
+        result.push(matrix[row][column])
+        matrix[row][column--] = null
+      }
+
+      row--
+      column++
+      nextMove = isNull() ? null : moves.get('left')
+    }
+
+    if (nextMove === 'up') {
+      while (row >= 0 && !isNull()) {
+        result.push(matrix[row][column])
+        matrix[row--][column] = null
+      }
+
+      row++
+      column++
+      nextMove = isNull() ? null : moves.get('up')
+    }
+  }
+
+  return result
 }
 
 console.log(
